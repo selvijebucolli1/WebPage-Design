293ae4560caf1c2ace5e512920db8e382c3517ec
selvijebucolli1 <selvije.bucoli87@gmail.com>
2022-12-05 13:42:15 +0100

    validime

---
+++ main.js
@@ -0,0 +1,10 @@
+$('.contact__button').click(function(e){
+    e.preventDefault();
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    if(name.length == 0 || email.length==0){
+        alert("Please fill name and email before submitting!");
+    }else {
+        alert("Form submitted succesfully!");
+    }
+})
